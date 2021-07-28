561d1643a08752b894f1f4bba2f2b2dcdc9f6cab
jrinconm <julianrinconm@gmail.com>
2021-07-28 16:47:49 +0200

    Create componente.vue
    Plantilla componente para copy&paste

---
+++ componente.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+  </div>
+</template>
+<script>
+//import { Componente } from "Component";
+export default {
+  name: "",
+  //components: { Componente },
+  /* props: {
+    propiedad: { required: true/false, type: Tipo },
+  },*/
+  data() {
+    return {
+    };
+  },
+  watch: {
+  },
+  computed: {
+  },
+  created() {
+  },
+  mounted() {
+  },
+  methods: {
+  }
+};
+</script>
+<style lang="scss" scoped>
+</style>
